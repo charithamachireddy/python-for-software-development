881166a563cff4f43ee716e1e1b4138719188f9b
Charitha Machireddy <charithamachireddy08@gmail.com>
2025-12-31 14:00:09 +0530

    added largest of 3 numbers program

---
+++ python-for-software-development/day04_variables_datatypes/largest_of_three.py
@@ -0,0 +1,14 @@
+# Program to find the largest of three numbers
+
+a = 12
+b = 45
+c = 30
+
+largest = a
+
+if b > largest:
+    largest = b
+if c > largest:
+    largest = c
+
+print("The largest number is:", largest)
