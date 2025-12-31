d5c4aa8d50446bb127c718c6d596b10b85b607a0
Charitha Machireddy <charithamachireddy08@gmail.com>
2025-12-31 13:59:10 +0530

    added small program

---
+++ python-for-software-development/day04_variables_datatypes/marks_total_average.py
@@ -0,0 +1,11 @@
+# Program to calculate total and average marks
+
+maths = 85
+science = 78
+english = 90
+
+total = maths + science + english
+average = total / 3
+
+print("Total marks:", total)
+print("Average marks:", average)
