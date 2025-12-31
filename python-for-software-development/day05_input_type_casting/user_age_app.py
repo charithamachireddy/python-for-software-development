be0c6bda898ac7183f29cfba0d71d53eed98a1dc
Charitha Machireddy <charithamachireddy08@gmail.com>
2025-12-31 14:22:51 +0530

    added day5

---
+++ python-for-software-development/day05_input_type_casting/user_age_app.py
@@ -0,0 +1,10 @@
+# Day 5: User Age Application
+# This program calculates age using birth year
+
+birth_year = input("Enter your birth year: ")
+birth_year = int(birth_year)
+
+current_year = 2025
+age = current_year - birth_year
+
+print("Your age is:", age)
