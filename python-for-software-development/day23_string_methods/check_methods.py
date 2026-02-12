30001ac29728af1a1ea977ef5341b9b8da65b828
Charitha Machireddy <charithamachireddy08@gmail.com>
2026-02-12 20:42:13 +0530

    Day 23: String methods examples

---
+++ python-for-software-development/day23_string_methods/check_methods.py
@@ -0,0 +1,6 @@
+text = "Python123"
+
+print(text.isalpha())
+print(text.isdigit())
+print(text.isalnum())
+print(text.startswith("Py"))
---
+++ python-for-software-development/day23_string_methods/split_join.py
@@ -0,0 +1,7 @@
+text = "Python is easy"
+
+words = text.split()
+print(words)
+
+joined = "-".join(words)
+print(joined)
